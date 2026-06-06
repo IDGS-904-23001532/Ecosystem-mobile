19a1425aa71d67c0f561bd41ab84f13fa9ecfeff
Christopher Isaac Rivera Valderrama <christoferriveravalderrama@gmail.com>
2026-06-06 01:56:37 -0600

    FIX: Se añadio la tarjetas y la llamo la img a la funcion principal ademas iniciando con el encabazado utilizando un item colum

--- app/src/main/java/com/example/ecosystem/MainActivity.kt
+++ app/src/main/java/com/example/ecosystem/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.example.ecosystem
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ecosystem.ui.theme.EcosystemTheme
+import com.example.ecosystem.ui.theme.colorNeutral
+import com.example.ecosystem.ui.theme.colorPrimario
+import com.example.ecosystem.ui.theme.colorSecundario
+import com.example.ecosystem.ui.theme.internormal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,25 +51,62 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EcosystemTheme {
-        Greeting("Android")
+
     }
 }
 
 @Composable
 fun inicio() {
 
+    ImagenFondoDecorativa()
+    LazyColumn() {
+        item{
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp)
+                        .background(colorNeutral)
+                ){
+                    Text(
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .align(alignment = Alignment.CenterHorizontally),
+                        text = "Ecosystem",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            color = colorPrimario,
+                            fontFamily = internormal
+                        )
+                    )
+                }
+            }
+
+
+
+        item{
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(text = "Título de la tarjeta", style = MaterialTheme.typography.titleLarge)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Este es el contenido principal de tu tarjeta utilizando Kotlin.")
+                }
+            }
+
+
+        }
+    }
 
 
 }
@@ -61,7 +119,7 @@ fun ImagenFondoDecorativa() {
             painter = painterResource(R.drawable.panel_solar),
             contentDescription = "Imagen de Fondo",
             contentScale = ContentScale.Crop, // Escala recortando los bordes excedentes
-            alpha = 0.15F, // Aplica un canal alfa del 15% para que no opaque los textos
+            alpha = 0.9F, // Aplica un canal alfa del 15% para que no opaque los textos
             modifier = Modifier.fillMaxSize()
         )
     }
