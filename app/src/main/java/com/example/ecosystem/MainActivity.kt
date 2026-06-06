8f60a22bc8fb1fcc8d1b3cf6e9cf3aa82c566dab
Christopher Isaac Rivera Valderrama <christoferriveravalderrama@gmail.com>
2026-06-06 00:28:20 -0600

    FIX: se añadio el la img para la pagina de inicio y ademas la funcion para la img de fondo

--- app/src/main/java/com/example/ecosystem/MainActivity.kt
+++ app/src/main/java/com/example/ecosystem/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ecosystem.ui.theme.EcosystemTheme
 
@@ -47,4 +51,18 @@ fun inicio() {
 
 
 
+}
+
+
+@Composable
+fun ImagenFondoDecorativa() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.panel_solar),
+            contentDescription = "Imagen de Fondo",
+            contentScale = ContentScale.Crop, // Escala recortando los bordes excedentes
+            alpha = 0.15F, // Aplica un canal alfa del 15% para que no opaque los textos
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
